1deaa237a142826319e59ad18baaa5c167c91efc
anu-bhav-18 <anubhav_23157128016@bpmcemadhepura.org>
2026-04-27 13:41:29 +0530

    Update solution.sql

--- LAB06/solution.sql
+++ LAB06/solution.sql
@@ -1,3 +1,10 @@
+# Copyright 2026 Anubhav Kumar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
 SELECT COUNT(*) AS total_students
 FROM STDUENT;
 
